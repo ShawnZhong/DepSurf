fbe2e4684fe8b58b7147bbdc3b00757f3adea18f
Shawn Zhong <github@shawnzhong.com>
2024-03-07 15:01:51 -0600

    Update

--- btf/depsurf/linux/download.py
+++ btf/depsurf/linux/download.py
@@ -1,26 +1,4 @@
-import logging
-
-
-def download(url, path):
-    if not path.exists():
-        import urllib.request
-
-        logging.info(f"Downloading {url} to {path}")
-        urllib.request.urlretrieve(url, path)
-    else:
-        logging.info(f"Using {path}")
-
-
-def unzip_gz(gz_path, result_path):
-    if not result_path.exists():
-        import gzip
-
-        logging.info(f"Unzipping {gz_path} to {result_path}")
-        with gzip.open(gz_path, "rb") as f_in:
-            with open(result_path, "wb") as f_out:
-                f_out.write(f_in.read())
-    else:
-        logging.info(f"Using {result_path}")
+from .utils import download, unzip_gz
 
 
 def download_package_index(package_url, result_path):
--- btf/depsurf/linux/version.py
+++ btf/depsurf/linux/version.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 def get_linux_version_tuple(name):
     """5.13.0-52-generic -> (5, 13, 0)"""
     return tuple(map(int, name.split("-")[0].split(".")))
@@ -8,3 +11,55 @@ def get_linux_version_short(name):
     t = get_linux_version_tuple(name)
     assert t[2] == 0
     return f"{t[0]}.{t[1]}"
+
+
+@dataclass(frozen=True)
+class UbuntuVersion:
+    version: str
+    arch: str
+
+    @property
+    def path(self):
+        from ..paths import DATA_PATH
+
+        return DATA_PATH / f"{self.version}-{self.arch}"
+
+    @property
+    def version_name(self):
+        return {
+            "16.04": "xenial",
+            "18.04": "bionic",
+            "20.04": "focal",
+            "22.04": "jammy",
+        }[self.version]
+
+    @property
+    def url_prefix(self):
+        return {
+            "x86": "http://security.ubuntu.com/ubuntu",
+            "arm64": "http://ports.ubuntu.com/ubuntu-ports",
+        }[self.arch]
+
+    @property
+    def url(self):
+        return f"{self.url_prefix}/dists/{self.version_name}-security/main/binary-{self.arch}/Packages.gz"
+
+    @property
+    def index_path(self):
+        return self.path / "index"
+
+    @property
+    def deb_path(self):
+        return self.path / "debs"
+
+    @property
+    def vmlinux_path(self):
+        return self.path / "vmlinux"
+
+    @property
+    def vmlinuz_path(self):
+        return self.path / "vmlinuz"
+
+    @property
+    def btf_path(self):
+        return self.path / "btf"
