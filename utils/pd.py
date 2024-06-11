21c8dd630aeec95c2500949afa4e2514d77dc31f
Shawn Zhong <github@shawnzhong.com>
2024-06-11 04:09:23 -0500

    Update

--- utils/pd.py
+++ utils/pd.py
@@ -1,13 +1,4 @@
-import logging
-
-
-from pathlib import Path
-from typing import Optional
-
 import pandas as pd
-
-
-from .paths import OUTPUT_PATH
 
 
 def setup_pandas():
@@ -20,41 +11,3 @@ def setup_pandas():
 
 
 setup_pandas()
-
-
-def save_df_txt(df: pd.DataFrame, path: Path):
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    df.to_string(path)
-    logging.info(f"Saved dataframe to {path}")
-
-    return df
-
-
-def save_df_pkl(df: pd.DataFrame, path: Path):
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    df.to_pickle(path)
-    logging.info(f"Saved dataframe to {path}")
-
-    return df
-
-
-def save_df(
-    df: pd.DataFrame, name: str, path: Path = OUTPUT_PATH, return_df=True
-) -> Optional[pd.DataFrame]:
-    save_df_txt(df, path=path / f"{name}.txt")
-    save_df_pkl(df, path=path / f"{name}.pkl")
-    if return_df:
-        return df
-
-
-def load_df(name, path=None) -> pd.DataFrame:
-    if path is None:
-        path = OUTPUT_PATH
-
-    filepath = path / f"{name}.pkl"
-    df = pd.read_pickle(filepath)
-    logging.info(f"Loaded df from {filepath}")
-
-    return df
