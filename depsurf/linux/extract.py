d1abbbbaa62e149f505432d31e332af7bf1540f8
Shawn Zhong <github@shawnzhong.com>
2024-06-27 01:31:49 -0500

    Update extract.py

--- depsurf/linux/extract.py
+++ depsurf/linux/extract.py
@@ -11,7 +11,7 @@ def list_deb(deb_path: Path):
 
 
 @check_result_path
-def extract_deb(deb_path: Path, file_path: Path, result_path: Path):
+def extract_deb(deb_path: Path, file_path: str, result_path: Path):
     logging.info(f"Extracting {file_path} in {deb_path} to {result_path}")
     # system(f"dpkg -x {deb_path} {tmp_path}")
     system(f"dpkg --fsys-tarfile {deb_path} | tar -xO .{file_path} > {result_path}")
