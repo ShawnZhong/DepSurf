392d0703bc198ef497c72f761e129930ba1709b4
Shawn Zhong <github@shawnzhong.com>
2025-02-04 12:07:23 +0800

    Update paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -6,9 +6,6 @@ PROJ_PATH = UTILS_PATH.parent
 OUTPUT_PATH = PROJ_PATH / "output"
 REPORT_PATH = OUTPUT_PATH / "report"
 
-OUTPUT_PATH.mkdir(exist_ok=True, parents=True)
-REPORT_PATH.mkdir(exist_ok=True, parents=True)
-
 PAPER_PATH = PROJ_PATH / "paper"
 TAB_PATH = PAPER_PATH / "tabs"
 FIG_PATH = PAPER_PATH / "figs"
@@ -20,7 +17,6 @@ BCC_TOOLS_PATH = BCC_PATH / "libbpf-tools"
 BCC_OBJ_PATH = BCC_TOOLS_PATH / ".output"
 
 TRACEE_PATH = SOFTWARE_PATH / "tracee"
-
 
 FONTS_PATH = UTILS_PATH / "fonts"
 FONT_MONO = FONTS_PATH / "Inconsolata_ExtraCondensed-Medium.ttf"
