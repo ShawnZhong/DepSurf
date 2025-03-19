70e5d42cd181c58d17ea1469303f5d7a165f046c
Shawn Zhong <github@shawnzhong.com>
2025-03-19 01:44:29 -0500

    Fix `system`

--- depsurf/utils/system.py
+++ depsurf/utils/system.py
@@ -9,7 +9,7 @@ def system(cmd, linux=False):
     if linux:
         if sys.platform == "darwin":
             cmd = f"orbctl run bash -c '{cmd}'"
-        else:
+        elif sys.platform != "linux":
             raise RuntimeError("Running linux command on non-Linux platform")
 
     logging.info(f'Running command: "{TermColor.OKGREEN}{cmd}{TermColor.ENDC}"')
